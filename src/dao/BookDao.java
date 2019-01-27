f02e6d2a55645c36f75b60e9ca04ed614b86ff2f
charlielei <lei_cheng2008@126.com>
2019-01-27 21:12:34 +0800

    更新Book

--- src/dao/BookDao.java
+++ src/dao/BookDao.java
@@ -3,8 +3,26 @@ package dao;
 import factory.ExcelFactory;
 import factory.PDFFactory;
 import factory.WordFactory;
+import model.Book;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BookDao {
+
+    private List<Book> bookList = new ArrayList<>();
+    private static BookDao instance = new BookDao();
+
+    public BookDao() {
+        bookList.add(new Book("book1", "book_1"));
+        bookList.add(new Book("book2", "book_2"));
+        bookList.add(new Book("book3", "book_3"));
+    }
+
+    public static BookDao getInstance() {
+        return instance;
+    }
+
     public void getReader(int kind) {
         if (kind ==1) {
             PDFFactory pdfFactory = new PDFFactory();
